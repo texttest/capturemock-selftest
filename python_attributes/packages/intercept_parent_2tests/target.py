8c9af879f7a3abe86d28e7dd828dd409fac1f3a4
Geoff Bache <geoff.bache@jeppesen.com>
2011-02-23 13:53:44 +0100

    Make it work to have multiple tests running in coded tests

---
+++ python_attributes/packages/intercept_parent_2tests/target.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from capturemock import capturemock, CaptureMockReplayError
+
+@capturemock("packagetomock")
+def test():
+    import packagetomock.sub.moduletomock
+    
+    print packagetomock.sub.moduletomock.call_function() + " " + packagetomock.sub.moduletomock.attribute
+
+@capturemock("packagetomock")
+def test2():
+    import packagetomock.sub.moduletomock
+    
+    print packagetomock.sub.moduletomock.attribute + " " + packagetomock.sub.moduletomock.call_function()
+
+try:
+    test()
+    test2()
+except CaptureMockReplayError:
+    import sys; sys.stderr.write(str(sys.exc_value) + "\n")
+    
