ee9128dc9aae105b50e40c072c6ddfe98950a35c
Geoff Bache <geoff.bache@jeppesen.com>
2011-03-01 21:03:03 +0100

    Make sure a subprocess can add to a parent process's file

---
+++ python_attributes/functional/subprocess_append/subproc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# ordinarily would be in some initialisation file
+from capturemock import process_startup
+process_startup()
+import moduletomock
+    
+print moduletomock.call_function()
