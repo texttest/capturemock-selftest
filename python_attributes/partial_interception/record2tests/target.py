169768b4eb58564a827dcc7973fe931a7edf6265
Geoff Bache <geoff.bache@jeppesen.com>
2011-03-03 20:47:31 +0100

    Check partial interception works with several tests

---
+++ python_attributes/partial_interception/record2tests/target.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from capturemock import capturemock, CaptureMockReplayError
+
+@capturemock(rcFiles=["capturemockrc"])
+def test():
+    import moduletomock
+    print moduletomock.attribute
+    print moduletomock.otherattribute
+    print moduletomock.method()
+    print moduletomock.othermethod()
+    print moduletomock.ClassName.staticMethod()
+    print moduletomock.ClassName.otherStaticMethod()
+    print moduletomock.ClassName().otherStaticMethod()
+
+    class MyClassName(moduletomock.OtherClassName):
+        pass
+
+    print MyClassName().method()
+
+
+@capturemock(rcFiles=["capturemockrc"])
+def test2():
+    import moduletomock
+    print moduletomock.attribute
+    print moduletomock.otherattribute
+    print moduletomock.method()
+
+
+try:
+    test()
+    test2()
+except CaptureMockReplayError:
+    import sys; sys.stderr.write(str(sys.exc_value) + "\n")
+    
