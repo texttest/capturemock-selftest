1a73e8cba5e73151ec69d23385b468462a341c73
Geoff Bache <geoff.bache@jeppesen.com>
2011-03-11 16:17:52 +0100

    Introduce a default location for capturemockrc file

---
+++ python_attributes/basic/replay_default_rcfile/target.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from capturemock import capturemock
+
+@capturemock
+def test():
+    import moduletomock
+    import smtplib # for testing additional filtering
+
+    print moduletomock.call_function() + " " + moduletomock.attribute
+
+try:
+    test()
+except:
+    import sys; sys.stderr.write(str(sys.exc_value) + "\n")
+    
