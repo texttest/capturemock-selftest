fb30ff1b75418e250721d989715a1780d40c7cfb
Geoff Bache <geoff.bache@jeppesen.com>
2011-03-22 16:59:00 +0100

    Adding support for not recording repeated functional calls

---
+++ python_attributes/functional/ignore_repeats/target.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from capturemock import capturemock, CaptureMockReplayError
+
+@capturemock(rcFiles=["capturemockrc"])
+def test():
+    import moduletomock
+    import smtplib # for testing additional filtering
+
+    print moduletomock.call_function() + " " + moduletomock.attribute
+    print moduletomock.call_function() + " " + moduletomock.attribute
+
+try:
+    test()
+except CaptureMockReplayError:
+    import sys; sys.stderr.write(str(sys.exc_value) + "\n")
+    
