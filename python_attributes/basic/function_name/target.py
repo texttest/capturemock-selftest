361a5a09c55c661d39305bb435cbb0c98fd542df
Geoff Bache <geoff.bache@jeppesen.com>
2011-04-07 22:08:06 +0200

    Preserve function name after decorating

---
+++ python_attributes/basic/function_name/target.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from capturemock import capturemock
+
+@capturemock("moduletomock")
+def test():
+    import moduletomock
+
+    print moduletomock.call_function() + " " + moduletomock.attribute
+
+try:
+    test()
+    print test.__name__
+except:
+    import sys; sys.stderr.write(str(sys.exc_value) + "\n")
+    
