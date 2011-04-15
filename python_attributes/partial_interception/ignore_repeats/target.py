b1c040659ffe740f0e54b64fdbffcabe0cc4ca82
Geoff Bache <geoff.bache@jeppesen.com>
2011-04-15 10:15:32 +0200

    Make sure ignoring repeats also works for partial interception

---
+++ python_attributes/partial_interception/ignore_repeats/target.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from capturemock import capturemock, CaptureMockReplayError
+
+@capturemock(rcFiles=["capturemockrc"])
+def test():
+    import moduletomock
+    print moduletomock.method()
+    print moduletomock.method()
+    
+try:
+    test()
+except CaptureMockReplayError:
+    import sys; sys.stderr.write(str(sys.exc_value) + "\n")
+    
