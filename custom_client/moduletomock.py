201c668d2b3d714ad0f4cde0508e75de92d166a4
Geoff Bache <geoff.bache@jeppesen.com>
2011-05-20 12:28:57 +0200

    Tests for new client functionality

---
+++ custom_client/moduletomock.py
@@ -0,0 +1,5 @@
+
+attribute = "REAL_ATTR"
+
+def call_function(*args):
+    return "REAL_RETURN"
---
+++ custom_client/target.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# We pretend we aren't working in Python, so we don't import capturemock
+
+import os, socket
+
+def sendText(text, serverAddress):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(serverAddress)
+    sock.sendall(text)
+    return sock
+
+def sendRecord(code, *args):
+    import moduletomock
+    sendText("SUT_CUSTOM:" + code + ":SUT_SEP:" + eval(code), *args)
+
+def printReplay(code, *args):
+    sock = sendText("SUT_CUSTOM:" + code, *args)
+    sock.shutdown(1)
+    print sock.makefile().read()
+    sock.close()
+
+def getServerAddress():
+    servAddr = os.getenv("CAPTUREMOCK_SERVER")
+    host, port = servAddr.split(":")
+    return host, int(port)
+
+address = getServerAddress()
+if os.getenv("CAPTUREMOCK_MODE") == "1":
+    # record
+    sendRecord("moduletomock.attribute", address)
+    sendRecord("moduletomock.call_function('hello')", address)
+else:
+    # replay
+    printReplay("moduletomock.attribute", address)
+    printReplay("moduletomock.call_function('hello')", address)
