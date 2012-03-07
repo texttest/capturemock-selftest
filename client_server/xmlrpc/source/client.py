c0b0d48e1febdb30a0c583fad2dce80d45d2bbe9
Geoff Bache <geoff.bache@jeppesen.com>
2012-03-07 13:21:58 +0100

    Handle multiline strings in lists, also multiple matches for the same alteration pattern with variable

--- client_server/xmlrpc/source/client.py
+++ client_server/xmlrpc/source/client.py
@@ -5,8 +5,10 @@ import xmlrpclib, sys
 servAddr = sys.argv[1]
 s = xmlrpclib.ServerProxy(servAddr)
 val = s.pow(2,3)  # Returns 2**3 = 8
-print val  
-print s.add(val,3)  # Returns 11
+print val
+val2 = s.pow(3,2) # Returns 3**2 = 9
+print val2
+print s.add(val,val2)  # Returns 17
 print s.add_newlines("") # test empty strings...
 try:
     print s.no_such_method("str")
