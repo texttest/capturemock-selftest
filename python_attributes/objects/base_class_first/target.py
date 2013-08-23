81fd256885e6d81a595f841690f43e10466aaa9b
Geoff Bache <geoff.bache@jeppesen.com>
2013-08-23 16:09:23 +0200

    Fixing MRO bug when base classes are intercepted before derived ones

---
+++ python_attributes/objects/base_class_first/target.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from capturemock import capturemock, CaptureMockReplayError
+
+@capturemock(rcFiles=["capturemockrc"])
+def test():
+    import moduletomock
+
+    baseclass = moduletomock.TheBaseClass
+    firstObj = baseclass()
+
+    obj = moduletomock.getObject("""Some Multiline
+String""")
+    if isinstance(obj, moduletomock.TheClass):
+        if isinstance(obj, baseclass):
+            print(obj.getNumber() + 1)
+            print("Nonsense attributes = " + repr(hasattr(obj, "nonsense")))
+        else:
+            print("Not the Base!")
+    else:
+        print("Not the Class!")
+
+
+try:
+    test()
+except CaptureMockReplayError:
+    import sys; sys.stderr.write(str(sys.exc_info()[1]) + "\n")
+
