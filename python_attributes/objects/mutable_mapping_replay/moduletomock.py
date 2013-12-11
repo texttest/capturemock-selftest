92f9c0d60783e1466c68defe84b1023e27c0c1e8
Marc Abramowitz <marc@marc-abramowitz.com>
2013-12-10 21:51:32 -0800

    Rename mutable_mapping to mutable_mapping_record. Add mutable_mapping_replay (currently failing).

---
+++ python_attributes/objects/mutable_mapping_replay/moduletomock.py
@@ -0,0 +1,15 @@
+from collections import MutableMapping
+
+class MyDict(MutableMapping):
+    def __init__(self, **kwargs):
+        self._dict = dict(kwargs)
+    def __delitem__(self):
+        del self._dict[key]
+    def __getitem__(self, key):
+        return self._dict[key]
+    def __setitem__(self, key, val):
+        self._dict[key] = val
+    def __iter__(self):
+        return iter(self._dict)
+    def __len__(self):
+        return len(self._dict)
