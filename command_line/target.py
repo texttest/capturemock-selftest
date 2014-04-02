8dc79b840dd626e573208ef7df2674701fe5e7fa
Geoff Bache <geoff.bache@jeppesen.com>
2014-04-02 10:38:13 +0200

    Modify test to add extra path elements at the end also

--- command_line/target.py
+++ command_line/target.py
@@ -15,7 +15,7 @@ os.system('firstcall -a -b -c "hello | goodbye"')
 
 # Check our environment and current working directory goes into the traffic environment
 os.environ["MY_ENV_VAR"] = "value"
-os.environ["PATH"] = "/some/extra/path" + os.pathsep + os.getenv("PATH")
+os.environ["PATH"] = "/some/extra/path" + os.pathsep + os.getenv("PATH") + os.pathsep + "/another/extra/path"
 del os.environ["MY_ENV_FOR_UNSET"]
 os.mkdir("tmpdir")
 os.chdir("tmpdir")
