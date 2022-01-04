5083e23600ec4ce4134abc9a2456f87349421a3f
Geoff Bache <geoff.bache@toyota-industries.eu>
2022-01-04 12:10:12 +0100

    Test for server replay directly from test harness code without starting CaptureMock server

---
+++ client_server/http/replay_server_from_python/target.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import subprocess, sys, os
+from urllib.request import urlopen
+from locale import getpreferredencoding
+import capturemock
+
+serverPath = os.path.join(os.getenv("TEXTTEST_SANDBOX"), "target_modules", "server.py")
+serverProc = subprocess.Popen([ sys.executable, serverPath ], stdout=subprocess.PIPE, stderr=open(os.devnull, "w"), cwd=os.path.expanduser("~"))
+serverAddress = str(serverProc.stdout.readline().strip().split()[-1], getpreferredencoding())
+capturemock.replay_for_server(serverAddress, *sys.argv[1:])
+try:
+    serverProc.terminate()
+except:
+    os.kill(serverProc.pid, signal.SIGTERM)
+
+serverProc.wait()
+
+
