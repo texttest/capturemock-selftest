34ea9cc08913abea2c0b3c32a3cab68df779e608
Geoff Bache <geoff.bache@pobox.com>
2022-01-07 11:09:39 +0100

    argument order changed

--- client_server/http/replay_server_from_python/target.py
+++ client_server/http/replay_server_from_python/target.py
@@ -8,7 +8,8 @@ import capturemock
 serverPath = os.path.join(os.getenv("TEXTTEST_SANDBOX"), "target_modules", "server.py")
 serverProc = subprocess.Popen([ sys.executable, serverPath ], stdout=subprocess.PIPE, stderr=open(os.devnull, "w"), cwd=os.path.expanduser("~"))
 serverAddress = str(serverProc.stdout.readline().strip().split()[-1], getpreferredencoding())
-capturemock.replay_for_server(serverAddress, *sys.argv[1:])
+args = sys.argv[1:] + [ serverAddress ]
+capturemock.replay_for_server(*args)
 try:
     serverProc.terminate()
 except:
